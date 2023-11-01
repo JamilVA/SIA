45401087c6eaa01a478cb782d4b74f484c84e94c
Jhan Carlos <HP@DESKTOP-5SP8KGD>
2023-11-01 11:06:52 -0500

    go front

--- server/controllers/estudiante.controller.js
+++ server/controllers/estudiante.controller.js
@@ -1,4 +1,3 @@
-const { response, request } = require('express');
 const Estudiante = require('../models/estudiante.model');
 const Persona = require('../models/persona.model');
 
@@ -14,40 +13,42 @@ const getEstudiante = async (req, res) => {
 }
 
 const crearEstudiante = async (req, res) => {
-    
-    const persona = await Persona.create(
-        {
-            codigo: null,
-            paterno: req.body.paterno,
-            materno: req.body.materno,
-            nombres: req.body.nombres,
-            rutaFoto: req.body.rutaFoto,
-            fechaNacimiento: req.body.fechaNacimiento,
-            sexo: req.body.sexo,
-            DNI: req.body.DNI,
-            email: req.body.email,
-        }
-    )
+    try {
+        const persona = await Persona.create(
+            {
+                codigo: null,
+                paterno: req.body.paterno,
+                materno: req.body.materno,
+                nombres: req.body.nombres,
+                rutaFoto: req.body.rutaFoto,
+                fechaNacimiento: req.body.fechaNacimiento,
+                sexo: req.body.sexo,
+                DNI: req.body.DNI,
+                email: req.body.email,
+            }
+        )
 
-    console.log("Código de la persona insertada: ", persona.codigo)
+        const estudiante = await Estudiante.create(
+            {
+                codigo: null,
+                codigoSunedu: req.body.codigoSunedu,
+                creditosLlevados: req.body.creditosLlevados,
+                creditosAprobados: req.body.creditosAprobados,
+                anioIngreso: new Date().getFullYear().toString(),
+                estado: true,
+                codigoPersona: persona.codigo
+            })
 
-    const estudiante = await Estudiante.create(
-        {
-            codigo: null,
-            codigoSunedu: req.body.codigoSunedu,
-            creditosLlevados: req.body.creditosLlevados,
-            creditosAprobados: req.body.creditosAprobados,
-            anioIngreso: new Date().getFullYear().toString(),
-            estado: true,
-            codigoPersona: persona.codigo
+        res.json({
+            "Estado": "Guardado con éxito",
+            persona,
+            estudiante
         })
-
-    res.json({
-        "Estado": "Guardado con éxito",
-        persona,
-        estudiante
-    })
-
+    } catch (error) {
+        res.json({
+            "Estado": "Error al guardar, " + error,
+        })
+    }
 }
 
 module.exports = {
