97068415593c4bf304f064d26f7dac1e709ffa7a
Jhan Carlos <HP@DESKTOP-5SP8KGD>
2023-10-31 19:02:56 -0500

    create controller student

--- server/controllers/estudiante.controller.js
+++ server/controllers/estudiante.controller.js
@@ -1,10 +1,8 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const Estudiante = require('../models/estudiante.model');
 const Persona = require('../models/persona.model');
 
 const getEstudiante = async (req, res) => {
-    //const desde = Number(req.query.desde) || 0;
-    //const limite = Number(req.query.limite) || 0;
     const estudiantes = await Estudiante.findAll({
         include: Persona
     });
@@ -15,6 +13,43 @@ const getEstudiante = async (req, res) => {
     });
 }
 
+const crearEstudiante = async (req, res = response) => {
+    const persona = await Persona.create(
+        {
+            paterno: req.body.paterno,
+            materno: req.body.materno,
+            nombres: req.body.nombres,
+            rutaFoto: req.body.rutaFoto,
+            fechaNacimiento: req.body.fechaNacimiento,
+            sexo: req.body.sexo,
+            DNI: req.body.DNI,
+            correo: req.body.correo,
+        }
+    )
+    const person = await persona.save()
+
+    /*const estudiante = await Estudiante.create(
+        {
+            codigoSunedu: req.body.codigoSunedu,
+            creditosLlevados: req.body.creditosLlevados,
+            creditosAprobados: req.body.creditosAprobados,
+            anioIngreso: new Date().getFullYear().toString(),
+            codigoPersona: person.codigo
+        }
+    )
+    await estudiante.save()*/
+
+    console.log(persona)
+
+    res.json({
+        ok: true,
+        persona,
+        //estudiante
+    });
+
+}
+
 module.exports = {
-    getEstudiante
+    getEstudiante,
+    crearEstudiante
 }
--- server/routes/estudiante.route.js
+++ server/routes/estudiante.route.js
@@ -1,14 +1,14 @@
 const { Router } = require('express');
 //const { check } =  require('express-validator');
 //const { validarCampos } = require('../middlewares/validar-campos');
-const { getEstudiante } = require('../controllers/estudiante.controller');
+const { getEstudiante, crearEstudiante } = require('../controllers/estudiante.controller');
 //const { validarJWT } = require ('../middlewares/validar-jwt');
 
 const router = Router();
 
 router.get('/', getEstudiante);
 
-//router.post('/', crearPersona);
+router.post('/', crearEstudiante);
 
 
 module.exports = router;
