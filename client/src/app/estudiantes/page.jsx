d13567d426606dfad68d1e8b2c655bbe8bff47a0
JamilVA <129433669+JamilVA@users.noreply.github.com>
2023-11-02 23:41:15 -0500

    Update page.jsx

--- client/src/app/estudiantes/page.jsx
+++ client/src/app/estudiantes/page.jsx
@@ -1,57 +1,63 @@
-'use client'
+"use client";
 
-import axios from 'axios';
-import React from 'react'
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import React from "react";
+import { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 const page = () => {
+  const [estudianteData, setEstudianteData] = useState([]);
 
-  const [estudianteData, setEstudianteData] = useState([])
-  
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
 
   const fetchData = async () => {
     try {
-      const result = await axios("http://localhost:3001/api/estudiante")
-      setEstudianteData(result.data.estudiantes)
-      console.log(result.data.estudiantes)
+      const result = await axios("http://localhost:3001/api/estudiante");
+      setEstudianteData(result.data.estudiantes);
+      console.log(result.data.estudiantes);
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   return (
-    <div>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Title</th>
-              <th>Content</th>
-              <th>Nombre</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              estudianteData.map((estudiante, i) => {
+    <Container>
+      <Card>
+        <Card.Header></Card.Header>
+        <Card.Body>
+          <Card.Title>Lista Estudiantes</Card.Title>
+          <Table striped bordered hover >
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Content</th>
+                <th>Nombre</th>
+              </tr>
+            </thead>
+            <tbody>
+              {estudianteData.map((estudiante, i) => {
                 return (
                   <tr key={i}>
-                    <td className='text-black-50'>{i + 1}</td>
-                    <td className='text-black-50'>{estudiante.codigoSunedu}</td>
-                    <td className='text-black-50'>{estudiante.anioIngreso}</td>
-                    <td className='text-black-50'>{estudiante.Persona.nombres}</td>
+                    <td className="text-black-50">{i + 1}</td>
+                    <td className="text-black-50">{estudiante.codigoSunedu}</td>
+                    <td className="text-black-50">{estudiante.anioIngreso}</td>
+                    <td className="text-black-50">
+                      {estudiante.Persona.nombres}
+                    </td>
                   </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-      </div>
-    </div>
-  )
-}
+                );
+              })}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
 
-export default page
+export default page;
