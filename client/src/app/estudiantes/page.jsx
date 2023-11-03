a1d70a236a35b99f77ee0a866de62d6972532c72
JamilVA <129433669+JamilVA@users.noreply.github.com>
2023-11-02 21:41:51 -0500

    Intento de colocar datos a la tabla

--- client/src/app/estudiantes/page.jsx
+++ client/src/app/estudiantes/page.jsx
@@ -1,50 +1,70 @@
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 
 async function getEstudiantes() {
-    const res = await fetch('http://localhost:3001/api/estudiante');
-    if (!res.ok) {
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error('Failed to fetch data')
-    }
+  const res = await fetch("http://localhost:3001/api/estudiante");
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
-    return res.json()
+  return res.json();
 }
 
 async function ListaEstudiantes() {
-    const data = await getEstudiantes()
-    console.log("Estudiantes: ", data)
-    return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Estudiate</th>
-                    <th>Código Sunedu</th>
-                    <th>DNI</th>
-                    <th>Correo</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td colSpan={2}>Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </Table>
-    );
+  const data = await getEstudiantes();
+  
+  const listaEstudiantes = [];
+  
+//   for (var i = 0; i < 9; i++) {
+//     n += i;
+//     mifuncion(n);
+//   }
+
+//   for (var propiedad in data.estudiantes) {
+//     if (data.estudiantes.hasOwnProperty(propiedad)) {
+//         console.log("hola")
+//       console.log(propiedad + ": " + data.estudiantes[propiedad]);
+//     }
+//   }
+
+  //   for (const [key, value] of Object.entries(data.estudiantes)) {
+  //     listaEstudiantes.push([`${key}`, `${value}`]);
+  //   }
+
+    console.log("Estudiantes: ", data.estudiantes);
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Estudiante</th>
+          <th>Código Sunedu</th>
+          <th>DNI</th>
+          <th>Correo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listaEstudiantes.map((val, key) => {
+          return (
+            <tr>
+              <td>{val.codigo}</td>
+              <td>{val.Persona}</td>
+              <td>{val.codigoSunedu}</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          );
+        })}
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
 }
 
 export default ListaEstudiantes;
