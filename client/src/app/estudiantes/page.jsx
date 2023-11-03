100db1422c6958f1ad0318352eb736bd70dc51eb
Jhan Carlos <HP@DESKTOP-5SP8KGD>
2023-11-02 23:15:09 -0500

    ya hay datos

--- client/src/app/estudiantes/page.jsx
+++ client/src/app/estudiantes/page.jsx
@@ -1,70 +1,57 @@
-import Table from "react-bootstrap/Table";
+'use client'
 
-async function getEstudiantes() {
-  const res = await fetch("http://localhost:3001/api/estudiante");
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+import axios from 'axios';
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+const page = () => {
+
+  const [estudianteData, setEstudianteData] = useState([])
+  
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const result = await axios("http://localhost:3001/api/estudiante")
+      setEstudianteData(result.data.estudiantes)
+      console.log(result.data.estudiantes)
+    } catch (e) {
+      console.log(e);
+    }
   }
 
-  return res.json();
-}
-
-async function ListaEstudiantes() {
-  const data = await getEstudiantes();
-  
-  const listaEstudiantes = [];
-  
-//   for (var i = 0; i < 9; i++) {
-//     n += i;
-//     mifuncion(n);
-//   }
-
-//   for (var propiedad in data.estudiantes) {
-//     if (data.estudiantes.hasOwnProperty(propiedad)) {
-//         console.log("hola")
-//       console.log(propiedad + ": " + data.estudiantes[propiedad]);
-//     }
-//   }
-
-  //   for (const [key, value] of Object.entries(data.estudiantes)) {
-  //     listaEstudiantes.push([`${key}`, `${value}`]);
-  //   }
-
-    console.log("Estudiantes: ", data.estudiantes);
-
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Estudiante</th>
-          <th>Código Sunedu</th>
-          <th>DNI</th>
-          <th>Correo</th>
-        </tr>
-      </thead>
-      <tbody>
-        {listaEstudiantes.map((val, key) => {
-          return (
+    <div>
+      <div>
+        <table>
+          <thead>
             <tr>
-              <td>{val.codigo}</td>
-              <td>{val.Persona}</td>
-              <td>{val.codigoSunedu}</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Content</th>
+              <th>Nombre</th>
             </tr>
-          );
-        })}
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-      </tbody>
-    </Table>
-  );
+          </thead>
+          <tbody>
+            {
+              estudianteData.map((estudiante, i) => {
+                return (
+                  <tr key={i}>
+                    <td className='text-black-50'>{i + 1}</td>
+                    <td className='text-black-50'>{estudiante.codigoSunedu}</td>
+                    <td className='text-black-50'>{estudiante.anioIngreso}</td>
+                    <td className='text-black-50'>{estudiante.Persona.nombres}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 
-export default ListaEstudiantes;
+export default page
