d289bfe148d9eba80a7df0eb59d599448c6fe175
EThawne <erickhuaman149@gmail.com>
2023-11-06 22:40:10 -0500

    Pagos

---
+++ server/models/conceptoPago.model.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const Pago = require('./pago.model')
+
+const ConceptoPago = sequelize.define('ConceptoPago', {
+    Codigo: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    Denominacion: DataTypes.STRING(25)
+})
+
+module.exports = ConceptoPago;
