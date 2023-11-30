7788cd7283216dfcd7c454e48271e4b5a1551ef1
JamilVA <129433669+JamilVA@users.noreply.github.com>
2023-11-30 09:22:29 -0500

    Asignanfo creacion de usuario para estudiante

--- server/controllers/estudiante.controller.js
+++ server/controllers/estudiante.controller.js
@@ -1,136 +1,152 @@
-const Estudiante = require('../models/estudiante.model');
-const Persona = require('../models/persona.model');
-const CarreraProfesional = require('../models/carreraProfesional.model');
-const { sequelize } = require('../config/database');
-const { QueryTypes } = require('sequelize');
+const Estudiante = require("../models/estudiante.model");
+const Persona = require("../models/persona.model");
+const CarreraProfesional = require("../models/carreraProfesional.model");
 
-Estudiante.belongsTo(Persona, { foreignKey: 'codigoPersona' })
-Persona.hasOne(Estudiante, {foreignKey: 'codigoPersona'})
-Estudiante.belongsTo(CarreraProfesional, { foreignKey: 'codigoCarreraProfesional' })
+const Usuario = require("../models/usuario.model");
+const NivelUsuario = require("../models/nivelUsuario.model");
+
+const { sequelize } = require("../config/database");
+const { QueryTypes } = require("sequelize");
+
+Persona.hasOne(Usuario, { foreignKey: "CodigoPersona" });
+Usuario.belongsTo(Persona, { foreignKey: "CodigoPersona" });
+
+Persona.hasOne(Estudiante, { foreignKey: "CodigoPersona" });
+Estudiante.belongsTo(Persona, { foreignKey: "CodigoPersona" });
+
+Estudiante.belongsTo(Persona, { foreignKey: "CodigoPersona" });
+Persona.hasOne(Estudiante, { foreignKey: "CodigoPersona" });
+Estudiante.belongsTo(CarreraProfesional, {
+  foreignKey: "CodigoCarreraProfesional",
+});
 
 const getEstudiante = async (req, res) => {
-    const estudiantes = await Estudiante.findAll({
-        include: [Persona, CarreraProfesional]
-    });
+  const estudiantes = await Estudiante.findAll({
+    include: [Persona, CarreraProfesional],
+  });
 
-    /*const estudiantes = await sequelize.query("SELECT E.Codigo, Estado, Paterno, Materno, Nombres, RutaFoto, AnioIngreso, FechaNacimiento, Sexo, DNI, Email, CodigoSunedu, CreditosLlevados, CreditosAprobados, CodigoCarreraProfesional, C.NombreCarrera, CodigoPersona FROM persona as P INNER JOIN estudiante as E ON E.CodigoPersona = P.Codigo INNER JOIN carreraprofesional as C ON E.CodigoCarreraProfesional = C.Codigo",
-        { Type: QueryTypes.SELECT })*/
-
-    res.json({
-        ok: true,
-        estudiantes,
-    });
-}
+  res.json({
+    ok: true,
+    estudiantes,
+  });
+};
 
 const crearEstudiante = async (req, res) => {
+  try {
+    const persona = await Persona.create({
+      Codigo: null,
+      Paterno: req.body.Paterno,
+      Materno: req.body.Materno,
+      Nombres: req.body.Nombres,
+      RutaFoto: req.body.RutaFoto,
+      FechaNacimiento: req.body.FechaNacimiento,
+      Sexo: req.body.Sexo,
+      DNI: req.body.DNI,
+      Email: req.body.Email,
+    });
 
-    try {
-        const persona = await Persona.create(
-            {
-                Codigo: null,
-                Paterno: req.body.Paterno,
-                Materno: req.body.Materno,
-                Nombres: req.body.Nombres,
-                RutaFoto: req.body.RutaFoto,
-                FechaNacimiento: req.body.FechaNacimiento,
-                Sexo: req.body.Sexo,
-                DNI: req.body.DNI,
-                Email: req.body.Email,
-            }
-        )
+    const estudiante = await Estudiante.create({
+      Codigo: null,
+      CodigoSunedu: req.body.CodigoSunedu,
+      CreditosLlevados: req.body.CreditosLlevados,
+      CreditosAprobados: req.body.CreditosAprobados,
+      AnioIngreso: new Date().getFullYear().toString(),
+      Estado: req.body.Estado,
+      CodigoPersona: persona.Codigo,
+      CodigoCarreraProfesional: req.body.CodigoCarreraProfesional,
+    });
 
-        const estudiante = await Estudiante.create(
-            {
-                Codigo: null,
-                CodigoSunedu: req.body.CodigoSunedu,
-                CreditosLlevados: req.body.CreditosLlevados,
-                CreditosAprobados: req.body.CreditosAprobados,
-                AnioIngreso: new Date().getFullYear().toString(),
-                Estado: req.body.Estado,
-                CodigoPersona: persona.Codigo,
-                CodigoCarreraProfesional: req.body.CodigoCarreraProfesional,
-            })
+    const usuario = await Usuario.create({
+      Codigo: null,
+      Estado: true,
+      CodigoPersona: persona.Codigo,
+      CodigoNivelUsuario: 3,
+    });
 
-        res.json({
-            "Estado": "Guardado con éxito",
-            persona,
-            estudiante
-        })
-    } catch (error) {
-        res.json({
-            "Estado": "Error",
-            "Error": error
-        })
-    }
-}
+    res.json({
+      Estado: "Guardado con éxito",
+      persona,
+      estudiante,
+      usuario
+    });
+  } catch (error) {
+    res.json({
+      Estado: "Error",
+      Error: error,
+    });
+  }
+};
 
 const actualizarEstudiante = async (req, res) => {
-    try {
-        const persona = await Persona.update(
-            {
-                Paterno: req.body.Paterno,
-                Materno: req.body.Materno,
-                Nombres: req.body.Nombres,
-                RutaFoto: req.body.RutaFoto,
-                FechaNacimiento: req.body.FechaNacimiento,
-                Sexo: req.body.Sexo,
-                DNI: req.body.DNI,
-                Email: req.body.Email,
-            }, {
-            where: {
-                Codigo: req.body.CodigoPersona,
-            }
-        }
-        )
+  try {
+    const persona = await Persona.update(
+      {
+        Paterno: req.body.Paterno,
+        Materno: req.body.Materno,
+        Nombres: req.body.Nombres,
+        RutaFoto: req.body.RutaFoto,
+        FechaNacimiento: req.body.FechaNacimiento,
+        Sexo: req.body.Sexo,
+        DNI: req.body.DNI,
+        Email: req.body.Email,
+      },
+      {
+        where: {
+          Codigo: req.body.CodigoPersona,
+        },
+      }
+    );
 
-        const estudiante = await Estudiante.update(
-            {
-                CodigoSunedu: req.body.CodigoSunedu,
-                CreditosLlevados: req.body.CreditosLlevados,
-                CreditosAprobados: req.body.CreditosAprobados,
-                AnioIngreso: new Date().getFullYear().toString(),
-                Estado: req.body.Estado,
-                CodigoCarreraProfesional: req.body.CodigoCarreraProfesional,
-            }, {
-            where: {
-                Codigo: req.body.Codigo,
-            }
-        })
+    const estudiante = await Estudiante.update(
+      {
+        CodigoSunedu: req.body.CodigoSunedu,
+        CreditosLlevados: req.body.CreditosLlevados,
+        CreditosAprobados: req.body.CreditosAprobados,
+        AnioIngreso: new Date().getFullYear().toString(),
+        Estado: req.body.Estado,
+        CodigoCarreraProfesional: req.body.CodigoCarreraProfesional,
+      },
+      {
+        where: {
+          Codigo: req.body.Codigo,
+        },
+      }
+    );
 
-        res.json({
-            "Estado": "Actualizado con éxito",
-            persona,
-            estudiante
-        })
-    } catch (error) {
-        res.json({
-            "Estado": "Error",
-            "Error": error
-        })
-    }
-}
+    res.json({
+      Estado: "Actualizado con éxito",
+      persona,
+      estudiante,
+    });
+  } catch (error) {
+    res.json({
+      Estado: "Error",
+      Error: error,
+    });
+  }
+};
 
 const buscarEstudiante = async (req, res) => {
-    try {
-        let estudiante = await Estudiante.findOne({
-            where: {
-                '$Persona.DNI$': req.query.dni
-            },
-            include: Persona
-        })
-    
-        res.json({
-            mensaje: "Encontrado",
-            estudiante
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    let estudiante = await Estudiante.findOne({
+      where: {
+        "$Persona.DNI$": req.query.dni,
+      },
+      include: Persona,
+    });
+
+    res.json({
+      mensaje: "Encontrado",
+      estudiante,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    getEstudiante,
-    crearEstudiante,
-    actualizarEstudiante,
-    buscarEstudiante
-}
+  getEstudiante,
+  crearEstudiante,
+  actualizarEstudiante,
+  buscarEstudiante,
+};
