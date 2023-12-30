c9efd3bc07ee36a2bee9c329d5754f5773e95110
JamilVA <129433669+JamilVA@users.noreply.github.com>
2023-12-29 19:32:27 -0500

    Modelo Sesion

---
+++ server/models/sesion.model.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+
+const Sesion = sequelize.define('Sesion', {
+    Codigo: {
+        type: DataTypes.STRING(12),
+        primaryKey: true
+    },
+    Numero: DataTypes.INTEGER,
+    Descripcion: DataTypes.STRING(45),
+    EstadoAsistencia: DataTypes.TINYINT,
+    LinkClaseVirtual: DataTypes.STRING(50),
+    Fecha: DataTypes.DATEONLY,
+    HoraInicio: DataTypes.TIME,
+    HoraFin: DataTypes.TIME,
+    EntradaDocente: DataTypes.TIME,
+    SalidaDocente: DataTypes.TIME,
+})
+
+module.exports = UnidadAcemica
