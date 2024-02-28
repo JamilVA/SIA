65147ae527437b8b9d8ed8a27c65ca35a78eeb46
EThawne <erickhuaman149@gmail.com>
2024-02-28 14:34:52 -0500

    Configuración de rutas protegidas

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,9 +1,15 @@
-import { withAuth } from "next-auth/middleware"
+export { default } from "next-auth/middleware";
 
-export default withAuth({
-    // Matches the pages config in [...nextauth]
-    pages: {
-        signIn: '/login',
-        error: '/error',
-    }
-})
+//Especifica las rutas que se van a proteger
+export const config = {
+    matcher: [
+        "/",
+        "/administrador/:path*", 
+        "/docente/:path*",
+        "/estudiante/:path*",
+        "/jefe/:path*",
+        "/manuales-usuario/:path*",
+        "/perfil/:path*",
+        "/tesoreria/:path*"
+    ]
+}
