a7cbd8c5a92f3fbcab257893b2bd427deb0649b9
EThawne <erickhuaman149@gmail.com>
2024-03-31 23:21:32 -0500

    Finaaaal

--- client/layout/AppFooter.tsx
+++ client/layout/AppFooter.tsx
@@ -1,11 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 
-import React, { useContext } from 'react';
-import { LayoutContext } from './context/layoutcontext';
+import React from 'react';
 
 const AppFooter = () => {
-    const { layoutConfig } = useContext(LayoutContext);
-
     return (
         <div className="layout-footer">
             <img src={`/images/pandalopers.png`} alt="Logo" height="20" className="mr-2" />
